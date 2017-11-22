089fa4f6cab233063ea7b894d48a0bd075274bc1
Roger Kikkas <roger.kikkas@gmail.com>
2017-11-22 12:26:39 +0200

    Added Person class

---
+++ src/ch/makery/address/model/Person.java
@@ -0,0 +1,16 @@
+package ch.makery.address.model;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Person extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
